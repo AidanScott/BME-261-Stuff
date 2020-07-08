c0b195e5c9f2a7765466f1fe6b95352d99a1683b
AidanScott <43943491+AidanScott@users.noreply.github.com>
2020-07-07 22:44:43 -0500

    Files for testing modules and implementations as needed
    messy, buggy and not well documented (sorry) most recent changes are testing for basic processing through scipy; successfully generated phonocardiogram from a heart sound sample and applied frequency domain transforms; next steps involve refactoring and transferring to the MFP folder, and moving on to filtering and feature extraction.

--- audiotesting for 261/audiotesting for 261/audiotesting_for_261.py
+++ audiotesting for 261/audiotesting for 261/audiotesting_for_261.py
@@ -5,6 +5,8 @@
 #scrap parser, figure out something else
 #queue and sys likely necessary
 #tempfile likely not?
+#time was used for testing, but isn't necessary for the actual project
+#not strictly certain sys is necessary either, but I don't want to fiddle with callback functions too much; I know this works, would rather not hcange it unless I have to
 
 import queue
 import sys
@@ -14,6 +16,8 @@ import time
 import sounddevice as sd
 import soundfile as sf
 import numpy as np  # Make sure NumPy is loaded before it is used in the callback
+import scipy as sp
+import matplotlib.pyplot as plt
 
 #import sockets
 #import tqdm
@@ -48,105 +52,77 @@ import numpy as np  # Make sure NumPy is loaded before it is used in the callbac
 #lets quickly sketch out a proof of concept of arbitrary recording length in the morning, but it should be chill
 #(I could just restrict recordings to 30 secs or something, but seems cheap)
 #lets switch back to the main file, and see if we can't get arbitrary duration going.
+
 #wait no
+
 # first test; while(stdin.empty())
+
 #no luck, guess we have to figure out async/await
-
-
-#async def user_input():
-#    input('Press enter to end recording.')
-#    return 1
-#
-#async def record_until():
-#    return await user_input()
-#
-#print(record_until())
-#    num = 0
-#    while(1 != record_until()):
-#        num+=1
-#    
-
-#loop = asyncio.get_event_loop()
-#
-#async def user_input():
-#    num = input('Enter number here: ')
-#    return num
-#
-#async def count_async(end):
-#    i = 0
-#    while(i < end):
-#        i+=1
-#    print(i)
-#    print('\n\n\n')
-#
-#async def count_if_input():
-#    input = await user_input()
-#    print(input)
-#    print('\n\n\n')
-#
-#def signal_handler(signal, frame):
-#    loop.stop()
-#    sys.exit(0)
-#
-#signal.signal(signal.SIGINT, signal_handler)
-#
-#asyncio.ensure_future(count_if_input())
-#asyncio.ensure_future(count_async(1000000))
-#loop.run_forever()
 
 #although I've learned a lot about asyncio, and it definitely seems useful; it seems to not be what I want here, unfortunately >>
 
 #Might have to learn threading, joy
 
-def thread_func(text):
-    print('Thread ', text, ': starting')
-    time.sleep(2)
-    print('Thread ',text,': finishing')
-
-x = threading.Thread(target=thread_func, args=(1,), daemon=True)
-print('pre-thread')
-x.start()
-print('post-thread')
+#def thread_func(text):
+#    print('Thread ', text, ': starting')
+#    time.sleep(2)
+#    print('Thread ',text,': finishing')
+#
+#x = threading.Thread(target=thread_func, args=(1,), daemon=True)
+#print('pre-thread')
+#x.start()
+#print('post-thread')
 
 #Hokay. :P
 
-#So. 
+#So. Threading works, that's low-level io out of the way. (Since the o part is pretty arbitrary, with obv exception to sockets and such, but we're not worrying about that yet, and possibly never will, we'll see. :P)
 
 #next big hurdle is going to be figuring out processing/analysis with scipy
 
-#q = queue.Queue()
+#let's start that, shall we?
 
+sample, sr = sf.read('Apex16.au')
 
-#def callback(indata, frames, time, status):
-#    """This is called (from a separate thread) for each audio block."""
-#    if status:
-#        print(status, file=sys.stderr)
-#    q.put(indata.copy())
+single_channel_sample = np.add(sample[:,0],sample[:,1])
 
+print(np.size(single_channel_sample))
 
-#try:
-#    if args.samplerate is None:
-#        device_info = sd.query_devices(args.device, 'input')
-#        # soundfile expects an int, sounddevice provides a float:
-#        args.samplerate = int(device_info['default_samplerate'])
-#    if args.filename is None:
-#        args.filename = tempfile.mktemp(prefix='delme_rec_unlimited_',
-#                                        suffix='.wav', dir='')
-#
-#    # Make sure the file is opened before recording anything:
-#    with sf.SoundFile(args.filename, mode='x', samplerate=args.samplerate,
-#                      channels=args.channels, subtype=args.subtype) as file:
-#        with sd.InputStream(samplerate=args.samplerate, device=args.device,
-#                            channels=args.channels, callback=callback):
-#            print('#' * 80)
-#            print('press Ctrl+C to stop the recording')
-#            print('#' * 80)
-#            while True:
-#                file.write(q.get())
-#except KeyboardInterrupt:
-#    print('\nRecording finished: ' + repr(args.filename))
-#    parser.exit(0)
-#except Exception as e:
-#    parser.exit(type(e).__name__ + ': ' + str(e))
+x = np.linspace(0.0,single_channel_sample.size/sr,single_channel_sample.size)
 
+plt.plot(x,single_channel_sample)
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+plt.grid()
+plt.show()
+plt.clf()
 
+sample_fft = sp.fft.fft(single_channel_sample)
+
+sample_power = np.abs(sample_fft)
+
+sample_freq = sp.fft.fftfreq(single_channel_sample.size, d=(1/sr))
+
+sample_x = np.linspace(0.0,1000,1000)
+
+#print(np.size(sample_fft,0))
+#print(np.size(sample_power,0))
+#print(np.size(sample_freq,0))
+#print(sample_power)
+
+plt.figure(figsize=(6,5))
+plt.plot(sample_freq[0:2000], 2.0/single_channel_sample.size * sample_power[0:2000])
+plt.xlabel('Frequency [Hz]')
+plt.ylabel('Power')
+plt.grid()
+plt.show()
+plt.clf()
+
+#num = 600
+#t = 1.0/800.0
+#x = np.linspace(0.0, num*t, num)
+#y = np.sin(50.0*2.0*np.pi*x) + 0.5*np.sin(80.0*2.0*np.pi*x)
+#y_fft = sp.fft.fft(y)
+#x_fft = np.linspace(0.0,1.0/(2.0*t),num//2)
+#plt.plot(x_fft,2.0/num * np.abs(y_fft[0:num//2]))
+#plt.grid()
+#plt.show()
